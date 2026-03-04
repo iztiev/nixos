645f13dc96c1a624366772f23f476c030409c2e9
Timur Izmagambetov <iztiev@gmail.com>
2026-03-04 17:12:08 +0500

    set Firefox as default browser

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -1,6 +1,21 @@
 { pkgs, inputs, ... }:
 
 {
+  # ── Default Browser ──
+  xdg.configFile."mimeapps.list".force = true;
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+    };
+  };
+
   # ── Firefox with Declarative Extensions ──
   programs.firefox = {
     enable = true;
